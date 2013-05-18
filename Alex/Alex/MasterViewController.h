74ff2952a681b5bf4b276fcfedc396c0da51b169
Alex <alex.oldenburg@speakap.nl>
2013-05-18 21:36:52 +0200

    updated git ignore

---
+++ Alex/Alex/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  Alex
+//
+//  Created by Alex on 5/13/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
---
+++ Alex/AlexTests/AlexTests.h
@@ -0,0 +1,13 @@
+//
+//  AlexTests.h
+//  AlexTests
+//
+//  Created by Alex on 5/13/13.
+//  Copyright (c) 2013 Alex. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AlexTests : SenTestCase
+
+@end
