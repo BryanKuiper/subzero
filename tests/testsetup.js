2b52bfcac2a4352458ebd66aea1072b346456da0
Alex <alex.oldenburg@speakap.nl>
2013-07-10 18:43:27 +0200

    added first test

--- tests/testsetup.js
+++ tests/testsetup.js
@@ -5,7 +5,7 @@
     var casper = require("casper").create();
     casper.start("http://localhost:4002/build/", function() {
 
-        casper.waitForSelector(".header-title", function() {
+        this.waitForSelector(".header-title", function() {
             this.test.assertTitle("Welcome at jsHeaven", "The header title is present");
         });
     });
